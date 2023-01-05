f31cc4eb93d52e311cebaec8f72a27bef9cae05b
ramre <ramre@ram>
2023-01-05 12:16:24 +0530

    gitexample

---
+++ src/demo2/demogit.java
@@ -0,0 +1,15 @@
+package demo2;
+
+public class demogit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=1;
+		int y=2;
+		int z;
+		z=x+y;
+		System.out.println(z);
+	}
+
+}
